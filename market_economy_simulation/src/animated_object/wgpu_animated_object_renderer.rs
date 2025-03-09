d95d95529cc801cd7bec209ef1036600154d9c1e
FirePrincess01 <SarahSteinbrecher01@gmx.at>
2025-03-09 21:00:28 +0100

    bigger terrain

--- market_economy_simulation/src/animated_object/wgpu_animated_object_renderer.rs
+++ market_economy_simulation/src/animated_object/wgpu_animated_object_renderer.rs
@@ -80,7 +80,7 @@ impl WgpuAnimatedObjectRenderer<'_> {
         let animation_uniform = deferred_animation_shader::AnimationUniform::zero();
 
         let instance = deferred_animation_shader::Instance {
-            position: [20.0, 20.0, 5.0],
+            position: [500.0, 20.0, 5.0],
             color: [0.5, 0.5, 0.8],
             entity: [99, 0, 0],
         };
--- market_economy_simulation_server/src/terrain.rs
+++ market_economy_simulation_server/src/terrain.rs
@@ -1,6 +1,6 @@
 #[derive(Clone)]
 pub struct Terrain {
-    pub heights: Vec<Vec<f32>>, // Two dimensional array of all heights
+    pub heights: Vec<f32>, // Two dimensional array of all heights
     pub distance: f32,          // Distance between two points
 
     pub size_x: usize,
@@ -8,33 +8,53 @@ pub struct Terrain {
 }
 
 impl Terrain {
-    pub fn new(steps_x: usize, steps_y: usize, distance: f32) -> Self {
+    pub fn new(size_x: usize, size_y: usize, distance: f32) -> Self {
         let amplitude = 10.0;
         let scale = 0.1;
 
         // user perlin noise to generate a terrain
-        let mut heights: Vec<Vec<f32>> = Vec::new();
+        let mut heights: Vec<f32> = Vec::new();
+        heights.resize(size_x * size_y, 0.0);
 
         let perlin = noise::Perlin::new(1);
-        for y in 0..steps_y {
-            let mut x_values: Vec<f32> = Vec::new();
-            for x in 0..steps_x {
-                let point_x = x as f64 * scale;
-                let point_y = y as f64 * scale;
 
-                let height = noise::NoiseFn::get(&perlin, [point_x, point_y]) * amplitude;
+        for y in 0..size_y {
+            for x in 0..size_x {
+                let mut height =  (noise::NoiseFn::get(&perlin, [x as f64 / 32.0, y as f64 / 32.0]) * 20.0).max(0.0) as f32;
+                height +=  (noise::NoiseFn::get(&perlin, [x as f64 / 16.0, y as f64 / 16.0]) * 10.0).max(0.0) as f32;
+                height +=  (noise::NoiseFn::get(&perlin, [x as f64 / 8.0, y as f64 / 8.0]) * 5.0).max(0.0) as f32;
+                height +=  (noise::NoiseFn::get(&perlin, [x as f64 / 4.0, y as f64 / 4.0]) * 2.0).max(0.0) as f32;
 
-                x_values.push((height) as f32);
-                // x_values.push(0.0);
+                // create canyon 
+                height = height * Terrain::canyon((x as f32 - size_x as f32 / 2.0) / 10.0);
+
+                heights[y * size_x + x] = height;
             }
-            heights.push(x_values);
         }
+   
+        // for y in 0..steps_y {
+        //     let mut x_values: Vec<f32> = Vec::new();
+        //     for x in 0..steps_x {
+        //         let point_x = x as f64 * scale;
+        //         let point_y = y as f64 * scale;
+
+        //         let height = noise::NoiseFn::get(&perlin, [point_x, point_y]) * amplitude;
+
+        //         x_values.push((height) as f32);
+        //         // x_values.push(0.0);
+        //     }
+        //     heights.push(x_values);
+        // }
 
         Self {
             heights,
             distance,
-            size_x: steps_x,
-            size_y: steps_y,
+            size_x,
+            size_y,
         }
+    }
+
+    fn canyon(x: f32) -> f32 {
+        1.0 - 1.0 / (1.0 + x *x *x *x *x *x)
     }
 }
